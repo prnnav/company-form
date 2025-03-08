30d7dfc9f193859b84548e5a45a8015bb757a0de
prnnav <149896834+prnnav@users.noreply.github.com>
2025-03-09 00:02:34 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+      './src/**/*.{js,jsx,ts,tsx}',  // All JSX and TSX files in the src folder
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
